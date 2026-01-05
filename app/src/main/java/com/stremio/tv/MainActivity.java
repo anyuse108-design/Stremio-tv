6f608a3888546abd2dcc3d1541095eff0e0ade2f
anyuse108-design <anyuse108@gmail.com>
2026-01-05 18:03:49 +0530

    Create java/com/stremio/tv/MainActivity.java

---
+++ app/src/main/java/com/stremio/tv/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stremio.tv;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.simple_list_item_1);
+    }
+}
